5db6849508657c3f99e7066c72c1858b57ccff51
d1149694026 <1149694026@qq.com>
2018-12-04 12:52:26 +0800

    mybatis

---
+++ src/main/java/com/dx/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.dx.dao;
+
+import com.dx.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
